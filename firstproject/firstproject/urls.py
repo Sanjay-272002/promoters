8d174d1aa10eb8b62b2930a76d0f57102be65ec2
Sanjay Kumar <sanjaykumaranna799@gmail.com>
2022-04-20 20:32:42 +0530

    fav

--- firstproject/firstproject/urls.py
+++ firstproject/firstproject/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 
 urlpatterns = [
@@ -28,8 +30,5 @@ urlpatterns = [
     path('accounts/',include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls')),
     path('socialaccounts/', include('allauth.urls')),
-
-
-
-    
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('static/favicon/favicon.ico'))),  
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
